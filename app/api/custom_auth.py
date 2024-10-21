4f36a5412d9ded0e46c8d11437986c1efdaaecc5
Almas Rakyshbaev <kasper10.0711@gmail.com>
2024-10-21 08:00:14 +0500

    добавлено время жизни токена( 1час )

--- app/api/custom_auth.py
+++ app/api/custom_auth.py
@@ -1,6 +1,8 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.authtoken.models import Token
+from django.utils import timezone
+from datetime import timedelta
 
 
 class TokenFromCookieAuthentication(BaseAuthentication):
@@ -13,5 +15,9 @@ class TokenFromCookieAuthentication(BaseAuthentication):
             token = Token.objects.get(key=token_key)
         except Token.DoesNotExist:
             raise AuthenticationFailed('Invalid token')
+
+        if token.created < timezone.now() - timedelta(hours=1):
+            token.delete()
+            raise AuthenticationFailed('Token has expired')
 
         return (token.user, token)
