3fbdebea3812c8906c900c902340b277c2ca68f7
Almas Rakyshbaev <kasper10.0711@gmail.com>
2024-11-18 08:32:04 +0500

    Удалил лишнее, добавил миксин для получения токена с куки

--- app/web_app/views/base.py
+++ app/web_app/views/base.py
@@ -1,45 +1,20 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+from api.custom_auth import TokenFromCookieAuthentication
 
 
-class IndexView(LoginRequiredMixin, TemplateView):
+class TokenRequiredMixin:
+    def dispatch(self, request, *args, **kwargs):
+        auth_result = TokenFromCookieAuthentication().authenticate(request)
+        if auth_result is None:
+            return JsonResponse({'detail': 'Учетные данные для аутентификации не были предоставлены'}, status=401)
+        user, token = auth_result
+        request.user = user
+        return super().dispatch(request, *args, **kwargs)
+
+
+class IndexView(TokenRequiredMixin, TemplateView):
     template_name = 'index.html'
     login_url = 'login'
     redirect_field_name = 'next'
-    # model = Article
-    # context_object_name = 'articles'
-    # ordering = ('created_at',)
-    # paginate_by = 2
-    # paginate_orphans = 1
 
-#     def get(self, request, *args, **kwargs):
-#         self.form = self.get_search_form()
-#         self.search_value = self.get_search_value()
-#         return super().get(request, *args, **kwargs)
-#
-#     def get_search_form(self):
-#         return SearchForm(self.request.GET)
-#
-#     def get_search_value(self):
-#         if self.form.is_valid():
-#             return self.form.cleaned_data['search']
-#         return None
-#
-#     def get_queryset(self):
-#         queryset = super().get_queryset().exclude(is_deleted=True)
-#         if self.search_value:
-#             query = Q(title__icontains=self.search_value) | Q(author__icontains=self.search_value)
-#             queryset = queryset.filter(query)
-#         return queryset
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(object_list=object_list, **kwargs)
-#         context['form'] = self.form
-#         context['favorite_form'] = FavoriteForm()
-#         if self.search_value:
-#             context['query'] = urlencode({'search': self.search_value})
-#         return context
-#
-#
-# class IndexRedirectView(RedirectView):
-#     pattern_name = 'index'
