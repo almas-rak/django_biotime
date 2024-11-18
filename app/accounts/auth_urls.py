e06bef4b389ec16868a57b988f69ee7585c947f7
Almas Rakyshbaev <kasper10.0711@gmail.com>
2024-11-18 08:26:10 +0500

    Добавлены ссылки на регистрацию и изменение пароля

--- app/accounts/auth_urls.py
+++ app/accounts/auth_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from accounts.views import LoginView
+from accounts.views import LoginView, ChangePasswordView, RegisterView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name='login'),
     path("logout/", LoginView.as_view(), name='logout'),
+    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
+    path('registration/', RegisterView.as_view(), name='registration'),
 ]
