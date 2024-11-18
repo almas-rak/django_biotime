323b401b92b5888f157cbd815d6585ab654e3df1
Almas Rakyshbaev <kasper10.0711@gmail.com>
2024-11-18 08:48:33 +0500

    Изменения:
    переделан класс LoginView для новой формы и проверка время жизни токена
    в логоут добавлено удаление токена
    добавлены представления смены пароля и регистрация

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,25 +1,33 @@
+from datetime import timedelta
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils import timezone
 from django.views.generic import TemplateView
 from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import AuthenticationFailed
 
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, PasswordChangeForm, RegisterForm
 
 
 class LoginView(TemplateView):
-    template_name = 'login_form.html'
-    form = LoginForm
+    template_name = 'form_base.html'
+    form_class = LoginForm
 
-    def get(self, request, *args, **kwargs):
-        form = self.form()
-        context = {'form': form}
-        return self.render_to_response(context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        context['class'] = "login-button"
+        context['btn'] = "Войти"
+        context['title'] = "Авторизация"
+        return context
 
     def post(self, request):
-        form = self.form(request.POST)
+        form = self.form_class(request.POST)
         if not form.is_valid():
             messages.error(request, "Некорректные данные")
             return redirect('login')
@@ -27,22 +35,30 @@ class LoginView(TemplateView):
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
 
+        try:
+            user = User.objects.get(username=username)
+            if not user.is_active:
+                messages.warning(request, "Ваш аккаунт не активен, обратитесь к администратору.")
+                return redirect('login')
+        except User.DoesNotExist:
+            pass  # Пользователь не существует, продолжаем аутентификацию
+
         user = authenticate(request, username=username, password=password)
         if not user:
-            messages.warning(request, "Пользователь не найден")
+            messages.error(request, "Неверное имя пользователя или пароль")
             return redirect('login')
 
-        login(request, user)
-
         token, created = Token.objects.get_or_create(user=user)
-
+        if token.created < timezone.now() - timedelta(hours=1):
+            token.delete()
+            token = Token.objects.create(user=user)
         response = HttpResponseRedirect(reverse('index'))
 
         response.set_cookie(
             'auth_token',
             token.key,  # Используем сгенерированный токен
             httponly=True,  # Защищает от доступа через JavaScript
-            secure=True,  # Устанавливается только через HTTPS
+            # secure=True,  # Устанавливается только через HTTPS
             samesite='Lax'  # Предотвращает отправку токена при запросах с других сайтов
         )
 
@@ -50,5 +66,74 @@ class LoginView(TemplateView):
 
 
 def logout_view(request):
-    logout(request)
-    return redirect('login')
+    token_key = request.COOKIES.get('auth_token')
+    if not token_key:
+        return HttpResponseRedirect('/auth/login/')  # Перенаправление при отсутствии токена
+
+    try:
+        token = Token.objects.get(key=token_key)
+        token.delete()  # Удаление токена из базы данных
+    except Token.DoesNotExist:
+        raise AuthenticationFailed('Invalid token')
+
+    logout(request)  # Завершаем сессию пользователя
+
+    response = HttpResponseRedirect('/auth/login/')
+    response.delete_cookie('auth_token')  # Удаление куки с токеном
+    response.delete_cookie('sessionid')  # Удаление куки с сессионным идентификатором
+
+    return response
+
+
+class ChangePasswordView(LoginRequiredMixin, TemplateView):
+    template_name = "form_base.html"
+    form_class = PasswordChangeForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return self.render_to_response({'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # Логика для смены пароля
+            old_password = form.cleaned_data['old_password']
+            new_password = form.cleaned_data['new_password']
+
+            if not request.user.check_password(old_password):
+                messages.error(request, "Старый пароль неверен.")
+            else:
+                request.user.set_password(new_password)
+                request.user.save()
+                messages.success(request, "Пароль успешно изменен.")
+                return redirect('index')  # Перенаправление на главную страницу
+
+        return self.render_to_response({'form': form})
+
+
+class RegisterView(TemplateView):
+    template_name = "form_base.html"
+    form_class = RegisterForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        context['class'] = "btn btn-danger"
+        context['btn'] = "Регистрация"
+        context['title'] = "Регистрация"
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(username=username, password=password)
+            user.is_active = False  # Устанавливаем учетную запись как неактивную
+            user.save()
+            messages.success(request, "Ваш аккаунт создан. Ожидайте активации.")
+            return redirect('login')
+
+        # Если форма недействительна, отобразите её снова с ошибками
+        return self.render_to_response(
+            {'form': form, 'class': "btn btn-danger", 'btn': "Регистрация", 'title': "Регистрация"})
