0b787446950b21d3e8e623a09d987013e503fe8f
Almas Rakyshbaev <kasper10.0711@gmail.com>
2024-11-18 08:32:48 +0500

    Добавлена форма регистрации и смены пароля

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -2,5 +2,52 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True, label='Логин')
-    password = forms.CharField(required=True, label='Пароль', widget=forms.PasswordInput)
+    username = forms.CharField(
+        required=True, 
+        widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Логин"}),
+        label='')
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "Пароль"}),
+        label=""
+    )
+
+
+class PasswordChangeForm(forms.Form):
+    old_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "Старый пароль"}),
+        label=""
+    )
+    new_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "Новый пароль"}),
+        label=""
+    )
+    confirm_new_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "Потверждение нового пароля"}),
+        label=""
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get('new_password')
+        confirm_new_password = cleaned_data.get('confirm_new_password')
+
+        if new_password != confirm_new_password:
+            raise forms.ValidationError("Новый пароль и его подтверждение не совпадают.")
+        return cleaned_data
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Имя пользователя"}),
+        label=""
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "Новый пароль"}),
+        label=""
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "Потверждение пароля"}),
+        label=""
+    )
